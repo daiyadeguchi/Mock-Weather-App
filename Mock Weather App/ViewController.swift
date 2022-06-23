73087a1d78467a3c0169ea2114a0ee1d46b997c8
daiyadeguchi <double.d.rugby@gmail.com>
2022-06-24 07:56:25 +0900

    Initial Commit

---
+++ Mock Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mock Weather App
+//
+//  Created by Daiya Deguchi on 2022/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
