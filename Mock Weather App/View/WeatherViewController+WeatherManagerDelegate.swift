164ceebfa3a1871823594b9ddd8b6b79516a132e
daiyadeguchi <daiyadeguchi@gmail.com>
2022-06-28 08:27:44 +0900

    Setup current weather view on top

--- Mock Weather App/View/WeatherViewController+WeatherManagerDelegate.swift
+++ Mock Weather App/View/WeatherViewController+WeatherManagerDelegate.swift
@@ -11,6 +11,15 @@ extension WeatherViewController: WeatherManagerDelegate {
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
         DispatchQueue.main.async {
             self.view.backgroundColor = weather.backgroundColor
+            
+            // Setup top/current weather view
+            let attributedText = NSMutableAttributedString(string: weather.cityName, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 25)])
+            attributedText.append(NSAttributedString(string: "\n\(Int(weather.temperature))°", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 70)]))
+            attributedText.append(NSAttributedString(string: "\n\(weather.conditionDescription)", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 25)]))
+            attributedText.append(NSAttributedString(string: "\nH:\(Int(weather.tempMax))° L:\(Int(weather.tempMin))°", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 25)]))
+            self.topLabel.attributedText = attributedText
+            self.topLabel.textAlignment = .center
+            
         }
     }
     
